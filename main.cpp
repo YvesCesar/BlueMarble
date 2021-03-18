f5e56decf25cf5dcdfaed7ced3b81883e0ac5700
Yves Amorim <yves@lepidus.com.br>
2021-03-18 11:15:39 -0400

    Projeto criado

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <stb_image.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main() 
+{
+    glm::vec3 V{ 1, 2, 3 };
+    std::cout << glm::to_string(V) << std::endl;
+    return 0;
+}

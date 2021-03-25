052892e7a3b973a0e7ea23d57dfdc4bc9993ac68
YvesCesar <yvesamorim73@gmail.com>
2021-03-25 10:42:19 -0400

    Create window

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 
-#include <glm/glm.hpp>
-#include <glm/gtx/string_cast.hpp>
-
-#include <stb_image.h>
-
-#include <GL/glew.h>
+#include <cassert>
 #include <GLFW/glfw3.h>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
 int main() 
 {
-    glm::vec3 V{ 1, 2, 3 };
-    std::cout << glm::to_string(V) << std::endl;
+    // Inicializar a biblioteca GLFW
+    assert(glfwInit());
+
+    // Criar uma janela
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Blue Marble", nullptr, nullptr);
+    assert(window);
+
+    // Entra no loop de eventos da aplicação
+    while (!glfwWindowShouldClose(window))
+    {
+        // glfwPollEvents() Processa todos os eventos na fila de eventos do GLFW
+        // Esses eventos podem ser do teclado, mouse, gamepad
+        glfwPollEvents();
+
+        // Envia o conteúdo do framebuffer da janela para ser desenhado na tela
+        glfwSwapBuffers(window);
+    }
+
+
+    // Encerra a biblioteca glfw
+    glfwTerminate();
     return 0;
 }

d090f2d4caef0abacf3ce70ce71ef1e3974e0841
YvesCesar <yvesamorim73@gmail.com>
2021-04-15 10:28:44 -0400

    Get GL_VERSION and GL_SHADING_LANGUAGE_VERSION

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ int main()
     // Usa o glGetString() para obter informações do driver
     std::cout << "OpenGL Vendor    : " << glGetString(GL_VENDOR) << std::endl;
     std::cout << "OpenGL Renderer  : " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL Version   : " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL Version     : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 
     // Entra no loop de eventos da aplicação
     while (!glfwWindowShouldClose(window))

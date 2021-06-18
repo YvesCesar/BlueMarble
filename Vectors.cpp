343be76e47d09763175d0d86d97108fa3429e6bb
YvesCesar <yvesamorim73@gmail.com>
2021-06-17 20:42:44 -0400

    Implements Point6 and Point7.

--- Vectors.cpp
+++ Vectors.cpp
@@ -14,13 +14,17 @@ void Constructors()
     glm::ivec2 Point3 { 3, 4 };
     glm::bvec3 Point4 { false, true, false };
     glm::dvec2 Point5 { 10.0, 10.0 }; // dvec2 possui precisão dupla(2x mais preciso em relação ao vec2)
+    glm::vec3 Point6 { Point0, 1.0f };
+    glm::vec2 Point7 { Point2 };
 
     std::cout << "Point0: " << glm::to_string(Point0) << std::endl;
     std::cout << "Point1: " << glm::to_string(Point1) << std::endl;
     std::cout << "Point2: " << glm::to_string(Point2) << std::endl;
     std::cout << "Point3: " << glm::to_string(Point3) << std::endl;
     std::cout << "Point4: " << glm::to_string(Point4) << std::endl;
-    std::cout << "Point5: " << glm::to_string(Point5) << std::endl; 
+    std::cout << "Point5: " << glm::to_string(Point5) << std::endl;
+    std::cout << "Point6: " << glm::to_string(Point6) << std::endl;
+    std::cout << "Point7: " << glm::to_string(Point7) << std::endl;  
 }
 
 int main() {

0324deb4416eb970cb2e223f7f9021bd888f4c63
YvesCesar <yvesamorim73@gmail.com>
2021-07-15 19:06:13 -0400

    Creates Triangle with OpenGL

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cassert>
+#include <array>
 
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -36,6 +39,25 @@ int main()
     std::cout << "OpenGL Version   : " << glGetString(GL_VERSION) << std::endl;
     std::cout << "GLSL Version     : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 
+    // Definir um triângulo em coordenadas normalizadas
+    std::array<glm::vec3, 3> Triangle = {
+        glm::vec3{ -1.0f, -1.0f, 0.0f },
+        glm::vec3{  1.0f, -1.0f, 0.0f },
+        glm::vec3{  0.0f,  1.0f, 0.0f }
+    };
+
+    // Agora, vamos copiar os vértices do triângulo para a memória da GPU
+    GLuint VertexBuffer;
+
+    // Pedir para o OpenGL gerar o identificador do VertexBuffer
+    glGenBuffers(1, &VertexBuffer);
+
+    // Ativar o VertexBuffer como sendo o buffer para onde vamos copiar os dados do triângulo
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+
+    // Copiar os dados para memória de vídeo
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle), Triangle.data(), GL_STATIC_DRAW);
+
     // Definir a cor de fundo 
     glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
@@ -46,6 +68,22 @@ int main()
         // Quando formos desenhar geometrias 3D, vamos voltar ao glClear porque teremos que limpar o buffer de profundidade (depth buffer)
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glEnableVertexAttribArray(0);
+
+        // glBindBuffer() Diz para o OpenGL que o VertexBuffer vai ser o buffer ativo do momento
+        glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+
+        // glVertexAttribPointer() Informa ao OpenGL onde, dentro do VertexBuffer, os vértices estão.
+        // No caso o array Triangles é contíguo em memória. Logo, basta dizer quantos vértices vamos usar para desenhar o triângulo
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        // glDrawArrays() diz para o OpenGL desenhar o triângulo com os dados que estão armazenados no VertexBuffer
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Reverter os estados criados
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDisableVertexAttribArray(0);
+
         // glfwPollEvents() Processa todos os eventos na fila de eventos do GLFW
         // Esses eventos podem ser do teclado, mouse, gamepad
         glfwPollEvents();
@@ -54,6 +92,8 @@ int main()
         glfwSwapBuffers(window);
     }
 
+    // Desalocar o VertexBuffer
+    glDeleteBuffers(1, &VertexBuffer);
 
     // Encerra a biblioteca glfw
     glfwTerminate();

3a1a14665ee0a519a514081e1f32bd74a62a4fe7
YvesCesar <yvesamorim73@gmail.com>
2021-07-27 20:57:23 -0400

    View and Projection with GLM.

--- Matrices.cpp
+++ Matrices.cpp
@@ -142,12 +142,55 @@ void ComposedMatrices()
     std::cout << glm::to_string(Direction) << std::endl;
 }
 
+void ModelViewProjection()
+{
+    std::cout << std::endl;
+    std::cout << "====================" << std::endl;
+    std::cout << " ModelViewProjection " << std::endl;
+    std::cout << "====================" << std::endl;
+
+    // Model vai ser a matriz formada pelas transformações de translação, rotação e escala. Logo, é uma matriz composta
+    glm::mat4 ModelMatrix = glm::identity<glm::mat4>();
+
+    // View 
+    glm::vec3 Eye{ 0, 0, 10 };
+    glm::vec3 Center{ 0, 0, 0 };
+    glm::vec3 Up{ 0, 1, 0 };
+    glm::mat4 ViewMatrix = glm::lookAt(Eye, Center, Up);
+
+    std::cout << "View" << std::endl;
+    PrintMatrix(ViewMatrix);
+
+    constexpr float FoV = glm::radians(45.0f);
+    const float AspectRatio = 800.0f / 600.0f;
+    const float Near = 1.0f;
+    const float Far = 100000.0f;
+    glm::mat4 ProjectionMatrix = glm::perspective(FoV, AspectRatio, Near, Far);
+
+    // Transformação Projetiva
+    std::cout << "Projection" << std::endl;
+    PrintMatrix(ProjectionMatrix);
+
+    glm::mat4 ModelViewProjection = ProjectionMatrix * ViewMatrix * ModelMatrix;
+
+    std::cout << "ModelViewProjection" << std::endl;
+    PrintMatrix(ModelViewProjection);
+
+    glm::vec4 Position{ 0, 0, 0, 1 };
+    Position = ModelViewProjection * Position;
+
+    Position = Position / Position.w;
+
+    std::cout << glm::to_string(Position) << std::endl;
+}
+
 int main() 
 {
     TranslationMatrix();
     ScaleMatrix();
     RotationMatrix();
     ComposedMatrices();
+    ModelViewProjection();
 
     return 0;
 }
